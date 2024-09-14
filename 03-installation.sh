5496f0287eb29da7e28b20ea84f785c7b9144eb1
koteshkunapuri <koteshkunapuri@gmail.com>
2024-09-15 02:16:41 +0530

    variables

--- 03-installation.sh
+++ 03-installation.sh
@@ -9,4 +9,13 @@ then
    exit 1
 fi
 
-dnf install git -y
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then
+  echo "Git is not installed, going to install it .."
+  dnf intsall git -y
+else 
+  echo "Git is already installed"
+  
+    
