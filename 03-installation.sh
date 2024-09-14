b011c26d67b41f42f07a2eb2855382482207688b
koteshkunapuri <koteshkunapuri@gmail.com>
2024-09-15 03:22:32 +0530

    variables

--- 03-installation.sh
+++ 03-installation.sh
@@ -9,15 +9,15 @@ then
    exit 1
 fi
 
-dnf list installed git 
+# dnf list installed git 
 
-if [ $? -ne 0 ]
-then
-    echo "git is not installed, going to install it .."
-    dnf install git -y
-else 
-    echo "git is already installed. Nothing to do...."
-fi    
+# if [ $? -ne 0 ]
+# then
+#     echo "git is not installed, going to install it .."
+#     dnf install git -y
+# else 
+#     echo "git is already installed. Nothing to do...."
+# fi    
 
 dnf list installed mysql
 
