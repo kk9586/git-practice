c6ea89407d7dd2b08865b0e14a61af478c6a5b1e
koteshkunapuri <koteshkunapuri@gmail.com>
2024-09-15 02:09:11 +0530

    variables

--- 03-installation.sh
+++ 03-installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-USERID=(id -u)
+USERID=$(id -u)
 echo "User Id is : $USERID"
-if[ $USERID -ne 0]
+if [ $USERID -ne 0]
 then 
    echo "please run this script with root priveleges"
    exit 1
