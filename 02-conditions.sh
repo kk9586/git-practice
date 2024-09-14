7e78c0203a9bf8be9cdda9eb532bf093f06a6a42
koteshkunapuri <koteshkunapuri@gmail.com>
2024-09-15 01:33:23 +0530

    variables

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if[ $NUMBER gt 20 ]
 then
-echo "given number is greater than 20"
+  echo "given number is greater than 20"
 else
-echo "given number is less than 20"
+  echo "given number is less than 20"
 fi
