c939150ab7a95fd2829b139a8aa4081f211bf901
koteshkunapuri <koteshkunapuri@gmail.com>
2025-05-21 23:29:20 +0530

    special variables

--- 02-conditions.sh
+++ 02-conditions.sh
@@ -2,9 +2,10 @@
 
 NUMBER=$1
 
-if [ $NUMBER gt 20 ]
+if [ $NUMBER gt 25 ]
 then
-  echo "given number is greater than 20"
+   echo "$NUMBER  is greater than 25"
+
 else
-  echo "given number is less than 20"
-fi
+   echo "NUMBER is less than 25" 
+fi     
