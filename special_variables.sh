85ce9634869c3adb7bf50314c61c20d651571fe7
koteshkunapuri <koteshkunapuri@gmail.com>
2025-05-21 23:02:08 +0530

    special variables

--- special_variables.sh
+++ special_variables.sh
@@ -6,5 +6,5 @@ echo "to know the name of a script : $0"
 echo "to know current working directory : $PWD"
 echo "to know home directory of current user : $HOME"
 echo "to know PID of the script executing now : $$"
-sleep 100 &
+# sleep 100 &
 echo "to know PID of the last background command : $!"
