dc5365f254f4340192912cf1968bad7859555e87
koteshkunapuri <koteshkunapuri@gmail.com>
2025-05-21 12:54:28 +0530

    this is just practicing the basics

--- variables.sh
+++ variables.sh
@@ -5,3 +5,9 @@ person2=mahesh
 
 echo "$person1:: hi $person2 how are you"
 echo "$person2:: hey hi $person1, iam fine. How are you doing"
+
+echo "enter the value"
+
+read $NUMBER
+
+echo "entered value is $NUMBER"
