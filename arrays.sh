a80011fb439aa8a75dadc066d7395f09ce62a0c2
koteshkunapuri <koteshkunapuri@gmail.com>
2025-05-21 15:32:14 +0530

    array practice

--- arrays.sh
+++ arrays.sh
@@ -3,3 +3,5 @@ fruits=(apple, orange, kiwi)
 echo "first fruit is : ${fruits[0]}"
 echo "second fruit is : ${fruits[1]}"
 echo "third fruit is : ${fruits[2]}"
+
+echo "all fruits are : ${fruits[@]}"
