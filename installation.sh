4fda2ecd92b7de7cafdf1b28b98dbe2fa3800eea
koteshkunapuri <koteshkunapuri@gmail.com>
2025-05-22 15:14:13 +0530

    sw installation

--- installation.sh
+++ installation.sh
@@ -65,6 +65,10 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then 
     echo "mysql is not installed, going to install now"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then 
+        echo "installing mysql"
 else
     echo "mysql is installed successfully"
 fi    
